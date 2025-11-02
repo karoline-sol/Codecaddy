31791e9e6828e60684896bb0101dd38aedff8eb2
karolinesoliman-pixel <karoline.soliman@gmail.com>
2025-11-02 14:27:53 -0500

    try vite base

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: "",
+  base: "https://karoline-sol.github.io/Codecaddy/docs",
   build: {
     outDir: 'docs',
     emptyOutDir: true, // also necessary
